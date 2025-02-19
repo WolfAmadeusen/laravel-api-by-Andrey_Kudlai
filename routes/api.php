54d2888ac2102c3b2e5fd971f00145fd14425dcd
Ridik <kobrinchuk3310@gmail.com>
2025-02-19 11:13:26 +0200

    Урок 9

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
@@ -21,9 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
    return $request->user();
 });
 
+Route::prefix('v1')->middleware(['throttle:api'])->group(function () {
+   Route::post('register', [AuthController::class, 'register']);
+   Route::post('login', [AuthController::class, 'login']);
+});
+
 
 Route::prefix('v1')->middleware(['throttle:api', 'auth:sactum'])->group(function () {
    // Одна строка — это сразу группа маршрутов
    Route::apiResource('categories', CategoryController::class);
    Route::apiResource('posts', PostController::class);
+
+   Route::get('logout', [AuthController::class, 'logout']);
 });
